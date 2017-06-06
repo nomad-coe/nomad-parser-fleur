8edd3c1a8dcc39b0577bf838b77e234970c90c14
Daria Tomecka <zarvok79@yahoo.com>
2017-06-06 23:48:52 +0200

    adding licence header

--- parser/parser-fleur/fleur_parser.py
+++ parser/parser-fleur/fleur_parser.py
@@ -12,6 +12,21 @@ import numpy as np
 # This is the parser for the main output file of Fleur (out) - adapted for version fleur26e
 ################################################################
 
+# Copyright 2016 The NOMAD Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 
 class FleurContext(object):
     """context for the fleur parser"""
