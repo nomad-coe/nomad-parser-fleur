294ac55bf4fcf1118d3c7679ba096cd3dc92df9b
Daria M. Tomecka <tomecka@fhi-berlin.mpg.de>
2018-04-26 18:41:18 +0200

    adapt copyright header

--- parser/parser-fleur/fleur_parser.py
+++ parser/parser-fleur/fleur_parser.py
@@ -8,11 +8,12 @@ import numpy as np
 #import fleur_parser_inp
 #import fleur_XML_parser
 
+
 ################################################################
 # This is the parser for the main output file of Fleur (out) - adapted for version fleur26e
 ################################################################
 
-# Copyright 2016-2018 The NOMAD Developers Group
+# Copyright 2016-2018 Daria M. Tomecka, Fawzi Mohamed
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
